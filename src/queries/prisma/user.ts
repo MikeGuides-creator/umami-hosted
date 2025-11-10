6852d264593bcb964382fe940883ee6882b9be92
Mike K. <mikeguidesblog@gmail.com>
2025-11-10 16:55:37 -0600

    Update user.ts

--- src/queries/prisma/user.ts
+++ src/queries/prisma/user.ts
@@ -1,22 +1,21 @@
-// src/queries/prisma/user.ts
-import type { Prisma } from '@prisma/client';
-import prisma from 'lib/prisma';
+import { Prisma } from "@prisma/client";
+import prisma from "lib/prisma";
 
-type GetUserOptions = {
+export interface GetUserOptions {
   includePassword?: boolean;
   showDeleted?: boolean;
-};
+}
 
 async function findUser(
-  criteria: Prisma.UserFindFirstArgs,
-  options: GetUserOptions = {},
+  criteria: Prisma.UserFindUniqueArgs,
+  options: GetUserOptions = {}
 ) {
   const { includePassword = false, showDeleted = false } = options;
 
   return prisma.client.user.findFirst({
     ...criteria,
     where: {
-      ...(criteria.where || {}),
+      ...criteria.where,
       ...(showDeleted ? {} : { deletedAt: null }),
     },
     select: {
@@ -29,10 +28,13 @@ async function findUser(
   });
 }
 
-export function getUserById(id: string, options: GetUserOptions = {}) {
-  return findUser({ where: { id } }, options);
+export async function getUser(userId: string, options: GetUserOptions = {}) {
+  return findUser({ where: { id: userId } }, options);
 }
 
-export function getUserByUsername(username: string, options: GetUserOptions = {}) {
+export async function getUserByUsername(
+  username: string,
+  options: GetUserOptions = {}
+) {
   return findUser({ where: { username } }, options);
 }
