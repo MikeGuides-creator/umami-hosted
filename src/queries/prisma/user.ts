bf25b3f6b26d2970150d21f3e24ca4504f758e34
Mike K. <mikeguidesblog@gmail.com>
2025-11-10 18:43:50 -0600

    fix: use relative prisma import for Vercel

--- src/queries/prisma/user.ts
+++ src/queries/prisma/user.ts
@@ -1,15 +1,12 @@
-import { Prisma } from "@prisma/client";
-import prisma from "lib/prisma";
+import { Prisma } from '@prisma/client';
+import prisma from '../../lib/prisma';
 
 export interface GetUserOptions {
   includePassword?: boolean;
   showDeleted?: boolean;
 }
 
-async function findUser(
-  criteria: Prisma.UserFindUniqueArgs,
-  options: GetUserOptions = {}
-) {
+async function findUser(criteria: Prisma.UserFindUniqueArgs, options: GetUserOptions = {}) {
   const { includePassword = false, showDeleted = false } = options;
 
   return prisma.client.user.findFirst({
@@ -32,9 +29,6 @@ export async function getUser(userId: string, options: GetUserOptions = {}) {
   return findUser({ where: { id: userId } }, options);
 }
 
-export async function getUserByUsername(
-  username: string,
-  options: GetUserOptions = {}
-) {
+export async function getUserByUsername(username: string, options: GetUserOptions = {}) {
   return findUser({ where: { username } }, options);
 }
